cde43532b1576559072907af869157aea357cf73
Vitkor <vitland@gmail.com>
2023-06-30 14:40:19 +0300

    feat: add GifContainer

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import Layout from './components/Layout/Layout'
 import Navbar from './components/Navbar/Navbar'
 import Searchbar from './components/Searchbar/Searchbar'
+import GifContainer from './components/GifContainer/GifContainer'
+
 const App = () => {
   return (
     <Layout>
       <Navbar />
       <Searchbar/>
+      <GifContainer/>
     </Layout>
   )
 }
