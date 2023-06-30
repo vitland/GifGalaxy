1b1c1d77e6143081dc8b1ec791fa0e2da377707c
Vitkor <vitland@gmail.com>
2023-06-30 14:04:25 +0300

    refactor: adaptive

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -5,21 +5,38 @@ import styles from './Searchbar.module.scss';
 
 const Searchbar = () => {
   const [value, setValue] = useState('');
+  const [isActive, setIsActive] = useState(false);
 
   const onChange = (e) => {
     setValue(e.target.value);
   };
+
   const onSubmit = (e) => {
     e.preventDefault();
   };
+
   const onClear = () => {
     setValue('');
   };
 
+  const toggleActive = () => {
+    setIsActive(!isActive);
+  };
+
   return (
-    <form className={styles.search} onSubmit={onSubmit}>
+    <form
+      className={
+        isActive ? `${styles.search} ${styles.active}` : `${styles.search}`
+      }
+      onSubmit={onSubmit}>
       <button type='submit'>
-        <FaMagnifyingGlass className={styles.searchBtn} />
+        <FaMagnifyingGlass
+          className={
+            isActive
+              ? `${styles.searchBtn} ${styles.active}`
+              : `${styles.searchBtn}`
+          }
+        />
       </button>
       <input
         type='text'
@@ -28,9 +45,17 @@ const Searchbar = () => {
         className={styles.input}
         onChange={onChange}
         placeholder='Поиск гиф'
+        onFocus={toggleActive}
+        onBlur={toggleActive}
       />
       <button type='button' onClick={onClear}>
-        <FaXmark className={styles.clearBtn} />
+        <FaXmark
+          className={
+            isActive
+              ? `${styles.clearBtn} ${styles.active}`
+              : `${styles.clearBtn}`
+          }
+        />
       </button>
     </form>
   );
